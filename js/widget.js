279caabcf50ded479efb46ca29e9e7033e940e73
Robin Drexler <drexler.robin@gmail.com>
2012-12-29 13:03:10 +0100

    add some anonymous tracking

--- js/widget.js
+++ js/widget.js
@@ -1,3 +1,5 @@
+var _gaq = _gaq || [];
+
 function resize() {
     var width  = $(window).width(),
         height = $(window).height();
@@ -9,16 +11,38 @@ function resize() {
 }
 
 $(function () {
+
+    /**
+     * Before we start to implement some fancy logic about when a user has really tried to search something
+     * and be before we're doing deferring and shit, we just assume, that focussing the input means, the user wants
+     * to search something :)
+     */
+    $('#search').on('focus', function() {
+        _gaq.push([ '_trackEvent', 'Widget', 'Search' ]);
+    });
+
     $('#search').bind('keydown', 'return', function () {
         var $firstItem = $('.mv-list .item:first'),
             url;
-
 
         if ($firstItem.length == 0) {
             return;
         }
 
         url = $firstItem.find('a').attr('href');
+
+        _gaq.push([ '_trackEvent', 'Widget', 'Enter' ]);
+
+        chrome.extension.sendMessage({purpose:"goto", url: url});
+    });
+
+    $('.mv-list').on('click', 'a.link', function(e) {
+        var url;
+        e.preventDefault();
+
+        url = $(this).attr('href');
+
+        _gaq.push([ '_trackEvent', 'Widget', 'Click_URL' ]);
         chrome.extension.sendMessage({purpose:"goto", url: url});
     });
 
@@ -27,3 +51,12 @@ $(function () {
     resize();
 
 });
+
+_gaq.push(['_setAccount', 'UA-33179945-6']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
