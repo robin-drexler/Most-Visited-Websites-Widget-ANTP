4cf1f75f81a683af86177a8a3ca946c00fb1c84f
Robin Drexler <drexler.robin@gmail.com>
2012-12-30 20:25:21 +0100

    track if users try to reach links via arrow keyboard keys

--- js/widget.js
+++ js/widget.js
@@ -1,27 +1,37 @@
 var _gaq = _gaq || [];
 
 function resize() {
-    var width  = $(window).width(),
+    var width = $(window).width(),
         height = $(window).height();
 
     $('body').css({
-        width : width,
-        height: height
+        width:width,
+        height:height
     });
 }
 
 $(function () {
-
+    var $search = $('#search');
     /**
-     * Before we start to implement some fancy logic about when a user has really tried to search something
-     * and be before we're doing deferring and shit, we just assume, that focussing the input means, the user wants
-     * to search something :)
+     * Before we start implementing some fancy logic about when a user really has tried to search something
+     * and be before we're doing deferring and shit, we just assume, that focusing the input expresses the
+     * users intend to search
      */
-    $('#search').on('focus', function() {
+    $search.on('focus', function () {
         _gaq.push([ '_trackEvent', 'Widget', 'Search' ]);
     });
 
-    $('#search').bind('keydown', 'return', function () {
+    /**
+     * Measure if people try to use arrow keys to navigate to links (after search)
+     */
+    $search.on('keydown', function (e) {
+        if (e.which == 38 || e.which == 40) {
+            _gaq.push([ '_trackEvent', 'Widget', 'Arrow_Key' ]);
+        }
+
+    });
+
+    $search.bind('keydown', 'return', function () {
         var $firstItem = $('.mv-list .item:first'),
             url;
 
@@ -33,17 +43,17 @@ $(function () {
 
         _gaq.push([ '_trackEvent', 'Widget', 'Enter' ]);
 
-        chrome.extension.sendMessage({purpose:"goto", url: url});
+        chrome.extension.sendMessage({purpose:"goto", url:url});
     });
 
-    $('.mv-list').on('click', 'a.link', function(e) {
+    $('.mv-list').on('click', 'a.link', function (e) {
         var url;
         e.preventDefault();
 
         url = $(this).attr('href');
 
         _gaq.push([ '_trackEvent', 'Widget', 'Click_URL' ]);
-        chrome.extension.sendMessage({purpose:"goto", url: url});
+        chrome.extension.sendMessage({purpose:"goto", url:url});
     });
 
     $(window).bind("resize", resize);
@@ -55,8 +65,11 @@ $(function () {
 _gaq.push(['_setAccount', 'UA-33179945-6']);
 _gaq.push(['_trackPageview']);
 
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+(function () {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
     ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
 })();
